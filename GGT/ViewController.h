a0ff0ce3b39f8e2bad7abab35ca6d817d073b032
Mickey Barboi <Damouse@Mickeys-Mac.local>
2014-02-05 11:02:06 -0800

    Initial Commit

---
+++ GGT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GGT
+//
+//  Created by Mickey Barboi on 2/5/14.
+//  Copyright (c) 2014 Mickey Barboi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
